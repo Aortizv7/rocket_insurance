17f88f6c083457d610f3e219816503ab888e687d
ana.ortiz <ana.ortiz.y.b.7@gmail.com>
2022-01-17 21:10:57 -0700

    adding select dropdowns

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
 
-
 //this app will have multiple steps, we need a form to submit the body in order to be able to request a quote
 // second thing is to show the drop down menu view to update the quote all while maintaining state
 //how to manage state? (redux/reduxify within the parent? )
@@ -23,7 +22,6 @@ const App = () => {
   const [quoteData, setQuoteData] = useState();
 
   const handleNameUpdate = (event) => {
-   
     const value = event.target.value;
     setQuoteForm({
       ...quoteForm,
@@ -32,7 +30,6 @@ const App = () => {
   };
 
   const handleAddressUpdate = (event) => {
- 
     const value = event.target.value;
 
     setQuoteForm({
@@ -45,94 +42,120 @@ const App = () => {
   };
 
   const handleFormSubmit = () => {
-    
-    axios.post('https://fed-challenge-api.sure.now.sh/api/v1/quotes',quoteForm).then(response => {
-      if(response.status === 200){
-        console.log(response.data.quote)
-        setQuoteData({...quoteData,...response.data.quote})
-      }
-    }).catch(error => console.log(error))
+    axios
+      .post("https://fed-challenge-api.sure.now.sh/api/v1/quotes", quoteForm)
+      .then((response) => {
+        if (response.status === 200) {
+          console.log(response.data.quote);
+          setQuoteData({ ...quoteData, ...response.data.quote });
+        }
+      })
+      .catch((error) => console.log(error));
 
-    setQuoteForm({
-      first_name: "",
-      last_name: "",
-      address: {
-        line_1: "",
-        line_2: "",
-        city: "",
-        region: "",
-        postal: "",
-      },
-    });
+    // setQuoteForm({
+    //   first_name: "",
+    //   last_name: "",
+    //   address: {
+    //     line_1: "",
+    //     line_2: "",
+    //     city: "",
+    //     region: "",
+    //     postal: "",
+    //   },
+    // });
   };
 
   return (
-    <div>
-      <label>
-        First Name
-        <input
-          type="text"
-          name="first_name"
-          value={quoteForm.first_name}
-          onChange={handleNameUpdate}
-        />
-      </label>
-      <label>
-        Last Name
-        <input
-          type="text"
-          name="last_name"
-          value={quoteForm.last_name}
-          onChange={handleNameUpdate}
-        />
-      </label>
-      <span>Address</span>
-      <label>
-        Line 1
-        <input
-          type="text"
-          name="line_1"
-          value={quoteForm.address.line_1}
-          onChange={handleAddressUpdate}
-        />
-      </label>
-      <label>
-        Line 2
-        <input
-          type="text"
-          name="line_2"
-          value={quoteForm.address.line_2}
-          onChange={handleAddressUpdate}
-        />
-      </label>
-      <label>
-        City
-        <input
-          type="text"
-          name="city"
-          value={quoteForm.address.city}
-          onChange={handleAddressUpdate}
-        />
-      </label>
-      <label>
-        Region
-        <input
-          type="text"
-          name="region"
-          value={quoteForm.address.region}
-          onChange={handleAddressUpdate}
-        />
-      </label>
-      <label>
-        Postal Code
-        <input
-          type="text"
-          name="postal"
-          value={quoteForm.address.postal}
-          onChange={handleAddressUpdate}
-        />
-      </label>
-      <button onClick={handleFormSubmit}>get free quote</button>
+    <div className="container">
+      <div className="form">
+        <label>
+          First Name
+          <input
+            type="text"
+            name="first_name"
+            value={quoteForm.first_name}
+            onChange={handleNameUpdate}
+          />
+        </label>
+        <label>
+          Last Name
+          <input
+            type="text"
+            name="last_name"
+            value={quoteForm.last_name}
+            onChange={handleNameUpdate}
+          />
+        </label>
+        <span>Address</span>
+        <label>
+          Line 1
+          <input
+            type="text"
+            name="line_1"
+            value={quoteForm.address.line_1}
+            onChange={handleAddressUpdate}
+          />
+        </label>
+        <label>
+          Line 2
+          <input
+            type="text"
+            name="line_2"
+            value={quoteForm.address.line_2}
+            onChange={handleAddressUpdate}
+          />
+        </label>
+        <label>
+          City
+          <input
+            type="text"
+            name="city"
+            value={quoteForm.address.city}
+            onChange={handleAddressUpdate}
+          />
+        </label>
+        <label>
+          Region
+          <input
+            type="text"
+            name="region"
+            value={quoteForm.address.region}
+            onChange={handleAddressUpdate}
+          />
+        </label>
+        <label>
+          Postal Code
+          <input
+            type="text"
+            name="postal"
+            value={quoteForm.address.postal}
+            onChange={handleAddressUpdate}
+          />
+        </label>
+        <button onClick={handleFormSubmit}>get free quote</button>
+      </div>
+      <div className="form">
+        <span>this will show the information</span>
+        {quoteData && (
+          <div>
+            <span>{quoteData.quoteId}</span>
+            <label>{quoteData.variable_options.deductible.title}</label>
+            <select>
+              {quoteData.variable_options.deductible.values.map((item) => (
+                <option key={item}>{item}</option>
+              ))}
+            </select>
+            <label>{quoteData.variable_options.asteroid_collision.title}</label>
+            <select>
+              {quoteData.variable_options.asteroid_collision.values.map(
+                (item) => (
+                  <option key={item}>{item}</option>
+                )
+              )}
+            </select>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
